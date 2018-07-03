0b34d886f864ec01d880a47747ea42c6c89986ca
NI GUO <guoni0909@gmail.com>
2018-07-03 00:11:12 -0700

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,58 @@
 $(document).ready(function(){
     
     var body = $("body");
+    body.append("<div class='container-fluid'><div class='text-center' id='imagesHolder'></div></div>");
     body.css("background-color","black");
 
-    body.append("<div class='container-fluid'><div class='text-center' id='imagesHolder'></div></div>");
-
-    body.append("<div id='displayImg'><div class='images'></div></div>");
-
-    $(".images").append("<button id='close'>close</button>");
-
-    $("#displayImg").append("<button id='left-narrow'><</button>");
-    $("#displayImg").append("<button id='right-narrow'>></button>");
+    body.append("<div class='container-fluid text-center' id='displayImg'><button id='close'>close</button></div>");
 
 
     var images = ["images/1.png", "images/2.jpg", "images/3.jpg", "images/4.jpg", "images/5.jpg", "images/6.jpg", "images/7.jpg", "images/8.jpg"];
 
     for(var i = 0; i < images.length; i++) {
-        $("#imagesHolder").append("<img src=" + images[i] + "></img>");
+        var image = "<img src=" + images[i] + "></img>";
+
+        $("#imagesHolder").append(image);
+
+        $("img").click(function(){
+            $("#displayImg").append(this);
+
+            $(this).css({
+                "width":"830px",
+                "height": "500px",
+                "margin-top":"40px"
+             });
+
+            $("button").css({
+                "display":"inline",
+                "color": "#fff",
+                "border":"1px solid #fff",
+                "border-radius":"3px",
+                "background":"rgba(0,0,0,0.1)",
+                "padding":"5px",
+                "right":"380px",
+                "top":"250px",
+                "position":"absolute"
+            });
+
+            $("#displayImg").click(function(){
+                $(this).remove();
+            });
+        });
+
     }
 
-    $("#displayImg").css({
-        "background-color": "rgba( 0, 0, 0, 0.8)",
-        "position": "fixed",
-        "top":"0",
-        "left":"0",
-        "height":"100%",
-        "width": "100%"
-        // "display":"none"
+    $("#imagesHolder").css({
+        "display":"block"
     });
 
-    $("#close").css({
-        "background-color": "rgba( 0, 0, 0, 0.8)",
-        "color": "#fff",
-        "border":"1px solid #fff",
-        "border-radius":"3px",
-        "padding":"5px",
-        "right":"280px",
-        "top":"100px",
-        "position":"absolute"
+    $("button").css({
+        "display":"none"
     });
-
-    $("#left-narrow").css({
-        "background-color": "rgba( 0, 0, 0, 0.8)",
-        "color": "#fff",
-        "border":"1px solid #fff",
-        "border-radius":"3px",
-        "padding":"8px",
-        "left":"350px",
-        "top":"350px",
-        "position":"absolute"
-    });
-
-    $("#right-narrow").css({
-        "background-color": "rgba( 0, 0, 0, 0.8)",
-        "color": "#fff",
-        "border":"1px solid #fff",
-        "border-radius":"3px",
-        "padding":"8px",
-        "right":"350px",
-        "top":"350px",
-        "position":"absolute"
-    });
-
-    // $("img").click(function(){
-
-    //     $(body).css({
-    //         "background-color": "black"
-    //     });
-
-    //     $("button").css({
-    //         "background-color": "red",
-    //         "color": "white"
-    //     });
-
-    // });
 
     $("img").css({
-        "width": "300px",
-        "height": "150px",
+        "width": "120px",
+        "height": "100px",
         "margin": "5px"
     });
 
